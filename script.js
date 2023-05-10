935e14a0a6bc6b25f2905a004514fdc2ce5954e5
DigambarGaikwad <104820408+DigambarGaikwad@users.noreply.github.com>
2023-05-10 11:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ const person = {
   age: 80,
 };
 
+
+const team = players;
+const team1 = [...players]; // Using spread operator to create a copy of players array
+
+const cap1 = { ...person }; // Using spread operator to create a copy of person object
+
+// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
+
+
+
+
+
+/*
+const players = ["John", "Bob", "Alice", "Poppy"];
+
+const person = {
+  name: "John Doe",
+  age: 80,
+};
+
 const team = players;
 const team1 = [...players]; // Using spread operator to create a copy of players array
 
@@ -14,3 +40,4 @@ const cap1 = { ...person }; // Using spread operator to create a copy of person 
 // console.log(team);   // Output: ["John", "Bob", "Alice", "Poppy"]
 // console.log(team1);  // Output: ["John", "Bob", "Alice", "Poppy"]
 // console.log(cap1);   // Output: { name: "John Doe", age: 80 }
+*/
